30da1b09923a9045c5ba2af03865813dee234e4a
david <zhangshibin1987@126.com>
2017-03-22 23:17:08 +0800

    基础工程调整

--- jshow-common/src/main/java/com/shareyi/jshow/utils/KaStringUtils.java
+++ jshow-common/src/main/java/com/shareyi/jshow/utils/KaStringUtils.java
@@ -1,13 +1,10 @@
 package com.shareyi.jshow.utils;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import com.shareyi.jshow.common.log.MarkConstant;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.util.CollectionUtils;
 
@@ -19,71 +16,7 @@ import org.springframework.util.CollectionUtils;
  */
 public class KaStringUtils {
 
-	/**
-	 * 是否自营实物商品,6-7位 sku
-	 * @param skuIds
-	 * @return
-	 */
-	public static boolean isSelfMaterialWare(String skuId){
-		if(StringUtils.isEmpty(skuId)){
-			return false;
-		}
-		return skuId.length()<8;
-	}
-	
-	public static boolean isSelfMaterialWare(Long skuId) {
-		if(skuId==null){
-			return false;
-		}
-		return skuId<1e8;
-	}
-	/**
-	 * 检查数据格式是否正确
-	 * @param skuIds
-	 * @return
-	 */
-	public static boolean isSkuIds(String skuIds){
-		Pattern pattern = Pattern.compile("^[\\d\\,]+");
-		Matcher matcher = pattern.matcher(skuIds);
-		return matcher.matches();
-	}
-	
-	
-	
-	/**
-	 * skuId数据转换,需要去重复
-	 * @param skuIds
-	 * @return
-	 */
-	public static List<Long> skuIdStrToList(String skuIds){
-		String[] skuIdArr=skuIds.split(",");
-		Set<Long> skuIdSet=new HashSet<Long>(skuIdArr.length);
-		for (String str : skuIdArr) {
-			if(StringUtils.isNotBlank(str)){
-				skuIdSet.add(Long.valueOf(str));
-			}
-		}
-		return new ArrayList<Long>(skuIdSet);
-	}
-	
-	
-	/**
-	 * skuId数据转换,内部操作，已经去重复，数据格式严谨
-	 * @param skuIds
-	 * @return
-	 */
-	public static List<String> skuIdStrToListStr(String skuIds) {
-		String[] skuIdArr=skuIds.split(",");
-		ArrayList<String> list=new ArrayList<String>(skuIdArr.length);
-		for (String str : skuIdArr) {
-			if(StringUtils.isNotBlank(str)){
-				list.add(str);
-			}
-		}
-		return list;
-	}
-	
-	
+
 	
 	/**
 	 * skuId数据转换,需要去重复
@@ -100,56 +33,6 @@ public class KaStringUtils {
 		}
 		sb.delete(sb.length()-seperator.length(), sb.length());
 		return sb.toString();
-	}
-
-    /**
-     * 过滤非法字符
-     * @return
-     */
-    public static String filterStr(String str){
-        if(StringUtils.isEmpty(str)){
-            return null;
-        }
-        String regEx = "[~!@#_%^&*()\\-|//[//]<>《》/～！＠＃￥％＆＊（）——|\\【】|a-z|A-Z|0-9|\\u4e00-\\u9fa5]";
-        Pattern p = Pattern.compile(regEx);
-        Matcher m = p.matcher(str);
-        String line="";
-        while(m.find()){
-            line=line+m.group();
-        }
-        return line;
-    }
-
-    public static String stringNumFilter(String str) {
-        try {
-            if(StringUtils.isEmpty(str)){
-                return null;
-            }
-            String regEx = "[0-9.]";
-            Pattern p = Pattern.compile(regEx);
-            Matcher m = p.matcher(str);
-            String line = "";
-            while (m.find()) {
-                line = line + m.group();
-            }
-            if (line.indexOf(".") != -1) {
-                line = line.substring(0, line.indexOf("."));
-            }
-            return line;
-        } catch (Exception e) {
-            return null;
-        }
-    }
-
-
-	
-	
-	public static void main(String[] args) {
-		System.out.println(isSkuIds("238834,,,,,,,,,,"));
-		System.out.println(isSkuIds("238834,238993,"));
-		System.out.println(isSkuIds("238834ds"));
-		System.out.println(isSkuIds("238834,dse"));
-
 	}
 
 
@@ -170,7 +53,7 @@ public class KaStringUtils {
 
 	/**
 	 * 将long的数据转换为String
-	 * @param idList
+	 * @param strList
 	 * @return
 	 */
 	public static List<Long> string2LongCollection(List<String> strList) {
@@ -182,6 +65,14 @@ public class KaStringUtils {
 	}
 
 
+	/**
+	 * 获取UUID
+	 * @return
+	 */
+	public static String getUUID(){
+		String uuid=UUID.randomUUID().toString().replace("-", "");
+		return uuid;
+	}
 
 
 
